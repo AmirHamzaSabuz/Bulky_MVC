08565aa5f3d9873856fc399ebfbaefa84455b271
Amir Hamza <hamza_amir@yahoo.com>
2023-07-06 12:50:12 +0600

    Section 2 - Data Annotations

--- Bulky_Web/Models/Category.cs
+++ Bulky_Web/Models/Category.cs
@@ -1,9 +1,16 @@
-﻿namespace Bulky_Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky_Web.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+
         public int DisplayOrder { get; set; }
     }
 }

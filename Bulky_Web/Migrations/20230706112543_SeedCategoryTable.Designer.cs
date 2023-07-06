e05cf5bde6d185cfdae4365f7b947f28618e7378
Amir Hamza <hamza_amir@yahoo.com>
2023-07-06 17:31:05 +0600

    Section 2 - Seed Category Table

--- Bulky_Web/Migrations/20230706112543_SeedCategoryTable.Designer.cs
+++ Bulky_Web/Migrations/20230706112543_SeedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using Bulky_Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bulky_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230706112543_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

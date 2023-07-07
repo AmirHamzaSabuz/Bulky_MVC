ce29dbf800eeb1d12d425eb559fb80d0e6e9fb83
Amir Hamza <hamza_amir@yahoo.com>
2023-07-07 11:32:56 +0600

    Section 2 - Update Category

--- Bulky_Web/Controllers/CategoryController.cs
+++ Bulky_Web/Controllers/CategoryController.cs
@@ -63,7 +63,7 @@ namespace Bulky_Web.Controllers
 
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }

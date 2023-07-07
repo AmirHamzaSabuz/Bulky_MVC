e816e4d56350a4871f62a1c4943d27daa18b8006
Amir Hamza <hamza_amir@yahoo.com>
2023-07-07 12:35:35 +0600

    Section 2 - TempData

--- Bulky_Web/Controllers/CategoryController.cs
+++ Bulky_Web/Controllers/CategoryController.cs
@@ -32,6 +32,7 @@ namespace Bulky_Web.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -66,6 +67,7 @@ namespace Bulky_Web.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -99,6 +101,7 @@ namespace Bulky_Web.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
